3b5bcfa96145acb74a05da09f09c1bb2177242e9
Jordan Rao <jordanrao@gmail.com>
2017-04-25 15:05:49 +0800

    added validations and shortener method to url model

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,18 @@
 class Url < ApplicationRecord
+  validates :long_url, presence: true
+  validates :long_url, uniqueness: true
+
+    before_create :counter, :shorten_url
+
+
+  def shorten_url
+    allchars = Array('A'..'Z') + Array('a'..'z') + Array(1..9)
+    extension = Array.new(8) { allchars.sample }.join
+    self.short_url = extension
+
+  end
+
+  def counter
+    self.click_count = 0
+  end
 end
