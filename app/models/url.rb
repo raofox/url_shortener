1e737d8fc17451120a28d75981f0fecaefbb2984
Jordan Rao <jordanrao@gmail.com>
2017-04-25 23:46:10 +0800

    add destroy controller action

--- app/models/url.rb
+++ app/models/url.rb
@@ -2,17 +2,18 @@ class Url < ApplicationRecord
   validates :long_url, presence: true
   validates :long_url, uniqueness: true
 
-    before_create :counter, :shorten_url
+    before_create :shorten
+    # before_create :counter
 
-
-  def shorten_url
+  def shorten
     allchars = Array('A'..'Z') + Array('a'..'z') + Array(1..9)
     extension = Array.new(8) { allchars.sample }.join
     self.short_url = extension
 
   end
+  #
+  # def counter
+  #   self.click_count = 0
+  # end
 
-  def counter
-    self.click_count = 0
-  end
 end
